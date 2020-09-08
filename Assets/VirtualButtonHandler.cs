8a5b4995e9a831cddd613a817d7540c513b9af6b
ArgentWaropod <62075349+ArgentWaropod@users.noreply.github.com>
2020-09-08 16:42:36 -0400

    Transfer

---
+++ Assets/VirtualButtonHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonHandler : MonoBehaviour//, IVirtualButtonEventHandler
+{
+    public GameObject button;
+    public GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /*public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+
+    }*/
+}
